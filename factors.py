616b06dbcbec8dedb9d3351e1274a49e7fbee710
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-04-17 23:50:19 -0700

    factors.py

--- factors.py
+++ factors.py
@@ -7,8 +7,8 @@ factors = []
 for i in range(1, int(integer**0.5) + 1):
     if integer % i == 0:
         factors.append(i)
-    if i * i != integer:
-        factors.append(integer // i)
+        if i != integer // i:
+            factors.append(integer // i)
     factors.sort()
 print(f"The factors of {integer} are: ")
 for factor in factors:
