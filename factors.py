d914363c09a7b93144f8df39cbc757878d65d266
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-04-17 23:43:06 -0700

    Add files via upload

---
+++ factors.py
@@ -0,0 +1,15 @@
+# Author: Van Huynh
+# GitHub username: huynhvan126
+# Date: 04/17/2024
+# Description: only enter positive integers and divide them evenly
+integer = int(input(f"Please enter a positive integer: "))
+factors = []
+for i in range(1, int(integer**0.5) + 1):
+    if integer % i == 0:
+        factors.append(i)
+    if i * i != integer:
+        factors.append(integer // i)
+    factors.sort()
+print(f"The factors of {integer} are: ")
+for factor in factors:
+    print(factor)
